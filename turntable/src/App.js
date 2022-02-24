68ba3af698a05686697761e2bb7125eed17e5154
Ayush Jain <ayujain2004@gmail.com>
2022-02-23 21:13:03 -0500

    update

--- turntable/src/App.js
+++ turntable/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import YoungArtist from "./youngArtist";
+
+
+
 
 function App() {
   return (
-    <TodoList />
+    <>
+    <button>Royalty Exchange</button>
+    <button>About</button>
+    <button>Young Artists</button>
+    <YoungArtist />
+    </>
+    
   );
 }
 
 export default App;
+// need image with text below 
---
+++ turntable/src/youngArtist.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class YoungArtist extends Component {
+  render() {
+    return (
+      <>
+      <div>
+  
+</div>
+      </>
+    
+     
+    )
+  }
+}
