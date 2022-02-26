f389e5fd542c0d200a08ec6ced686dd6c6bbf3cc
Ayush Jain <ayujain2004@gmail.com>
2022-02-26 00:26:56 -0500

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,26 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-
 import firebase from "firebase"; 
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
+
+ReactDOM.render(
+  
+    <App />,
+    document.getElementById('root')
+);
+
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -37,14 +32,6 @@ const firebaseConfig = {
   appId: "1:807663333425:web:0f5996f997850ae58f2510",
   measurementId: "G-PZVJBJN05F"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-//above is firebase init
-//below is setting up the backend w/ express and Node
 
 export var url = "http://localhost:3000"
 
