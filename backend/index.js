9957906b64a6c8d58da9e15ee06b26806f620b7d
Ayush Jain <ayujain2004@gmail.com>
2022-02-26 00:14:11 -0500

    update

--- backend/index.js
+++ backend/index.js
@@ -19,3 +19,12 @@ app.post('/', function(req,res){
 app.listen(3000, ()=>
 console.log(`Example app listening on port 30001`), 
 );
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://turntable-6bf59-default-rtdb.firebaseio.com"
+});
