d6fd98b01422c458afa197a859d04161c5504a68
Ayush Jain <ayujain2004@gmail.com>
2022-03-06 10:13:33 -0500

    update

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,26 @@ import React from "react"
 import Signup from "./Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <AuthProvider>
+    
 <Container className = "d-flex align-items-center justify-content-center"
     style = {{ minHeight: "100vh" }}>
       <div className = "w-100" style = {{ maxWidth: "400px" }}>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path ="/signup" component = {Signup}/>
+          </Routes>
+
+        </AuthProvider>
+      </Router>
       <Signup/>
         </div>
     </Container>
-    </AuthProvider>
     
-  
  
   )
 }
