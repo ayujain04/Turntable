51f48438845ad31e57b09ec004bc7970d5192f97
Ayush Jain <ayujain2004@gmail.com>
2022-03-05 20:45:05 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="application">
+      <h1>Apply to join TurnTable!</h1>
+      <form>
+      <fieldset>
+         <label>
+           <p>Name of Music Group</p>
+           <input name="name" />
+         </label>
+       </fieldset>
+       <fieldset>
+         <label>
+           <p>How many years have you been an artist?</p>
+           <input years="years" />
+         </label>
+       </fieldset>
+       <fieldset>
+         <label>
+           <p>What is your artist email?</p>
+           <input email="email" />
+         </label>
+       </fieldset>
+       <fieldset>
+         <label>
+           <p className = "outro">Our team will contact you within 48 hours to see if you are eligible. Thank you for joining the TurnTable Team!</p>
+         </label>
+       </fieldset>
+
+       <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
+
