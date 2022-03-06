9eeab24582c3969437bca8eacaabb6215087f91c
Ayush Jain <ayujain2004@gmail.com>
2022-03-05 21:30:14 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { initializeApp } from 'firebase/app';
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
 
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+const app = initializeApp(firebaseConfig);
