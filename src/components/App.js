36e352d39c424de1388d515a21c2da028dd56dbc
Ayush Jain <ayujain2004@gmail.com>
2022-03-06 00:13:04 -0500

    attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,39 @@
 
-import React from 'react';
-import './App.css';
+import React from "react"
 import Signup from "./Signup"
-import { Container } from 'react-bootstrap'
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "../contexts/AuthContext"
+import { BrowserRouter as Switch, Router, Route } from 'react-router-dom';
+import Dashboard from "./Dashboard"
+import Login from "./Login"
+import PrivateRoute from "./PrivateRoute"
+import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
- <Container className = "d-flex align-items-center justif-content-center"
- style ={{minHeight: "100vh"}}>
-   <div className = "w-100" styel = {{maxWidth: "400px"}}>
-   <Signup/>
-     </div>
-  
- </Container>
- 
-
-  
-  );
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
 }
 
-export default App;
+export default App
 
 /*
     <>
