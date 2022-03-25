adf3ea8fb9aec74efcac039942902fde462fa0d6
Ayush Jain <ayujain2004@gmail.com>
2022-03-24 23:04:47 -0400

    figured out how to update realtime database

--- src/components/ArtistApplication.js
+++ src/components/ArtistApplication.js
@@ -1,70 +1,169 @@
 import React, { useState } from 'react'
-import { Axios, db } from '../firebase'
-import './styled.scss'
+import "./ArtistApplication.scss"
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import app from "../firebase"
 
-export default function ContactForm() {
-  const [formData, setFormData] = useState({})
 
-  const updateInput = e => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    })
-  }
-  const handleSubmit = event => {
-    event.preventDefault()
-    sendEmail()
-    setFormData({
-      name: '',
-      email: '',
-      message: '',
-    })
-  }
-  const sendEmail = () => {
-    Axios.post(
-      'https://us-central1-turntable-6a455.cloudfunctions.net/submit',
-      formData
-    )
-      .then(res => {
-        db.collection('emails').add({
-          name: formData.name,
-          email: formData.email,
-          message: formData.message,
-          time: new Date(),
-        })
-      })
-      .catch(error => {
-        console.log(error)
-      })
+
+export default function ArtistApplication() {
+  
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    subject: "", 
+    message: ""
+  })
+
+  const {name, email, subject, message} = state; 
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      if(!name || !email || !subject || !message) {
+        toast.error("Please provide a value in each input field")
+      } else {
+        app.child("contacts").push(state);
+        setState({name: "", email: "", subject: "", message: ""}); 
+        toast.success("Thank you! The form was submitted successfully!")
+      }
   }
 
+  const handleInputChange = (e) => {
+
+    let{name, value} = e.target;
+    setState({...state, [name]: value});
+
+
+  }
+  
+  
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={updateInput}
-          value={formData.name || ''}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={updateInput}
-          value={formData.email || ''}
-        />
-        <textarea
-          type="text"
-          name="message"
-          placeholder="Message"
-          onChange={updateInput}
-          value={formData.message || ''}
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
-    </>
+   <section className = "contact-section">
+      <div className = "container">
+        <ToastContainer position = "top-center"/>
+        <div className = "row justify-content-center">
+            <div className='col-md-10'>
+              <div className = "wrapper">
+                <div className = "row no-gutters">
+                  <div className = "col-md-6">
+                      <div className = "contact-wrap w-100 p-lg-5 p-4">
+                        <h3 className = "mb-4">Send us a message</h3>
+                        <form id = "contactForm" className = "contactForm" onSubmit = {handleSubmit} >
+                              <div className="row">
+                                <div className ="col-md-12">
+                                    <div className = "form-group">
+                                      <input 
+                                      type = "text"
+                                      className = "form-control"
+                                      name = "name"
+                                      placeholder = "Name"
+                                      onChange = {handleInputChange}
+                                      value = {name}
+                                      />
+                                      </div>
+                                  </div>
+                                  <div className ="col-md-12">
+                                    <div className = "form-group">
+                                      <input 
+                                      type = "email"
+                                      className = "form-control"
+                                      name = "email"
+                                      placeholder = "Email"
+                                      onChange = {handleInputChange}
+                                      value = {email}
+                                      />
+                                      </div>
+                                  </div>
+                                  <div className ="col-md-12">
+                                    <div className = "form-group">
+                                      <input 
+                                      type = "text"
+                                      className = "form-control"
+                                      name = "subject"
+                                      placeholder = "Subject"
+                                      onChange = {handleInputChange}
+                                      value = {subject}
+                                      />
+                                      </div>
+                                  </div>
+                                  <div className ="col-md-12">
+                                    <div className = "form-group">
+                                      <textarea 
+                                      type = "text"
+                                      className = "form-control"
+                                      name = "message"
+                                      placeholder = "Message"
+                                      cols = "30"
+                                      rows = "6"
+                                      onChange = {handleInputChange}
+                                      value = {message}
+                                      ></textarea>
+                                      </div>
+                                  </div>
+                                  <div className='col-md-12'>
+                                      <div className = "form-group">
+                                        <input  type = "submit" value = "Send Message" className = "btn btn-primary" />
+                                        </div>
+                                    </div>
+                                </div>
+                        </form>
+                        </div>
+                    </div>
+                    <div className = "col-md-6 d-flex align-items-stretch">
+                      <div className = "info-wrap w-100 p-lg-5 p-4 img">
+                        <h3>Contact us</h3>
+                        <p className = "mb-4">We're open for any suggestions or just to have a chat.</p>
+                        <div className = "dbox w-100 d-flex align-items-start">
+                         <div className = "icon d-flex align-items-center justify-content-center">
+                           <span className = "fa fa-map-marker"></span>
+                           </div>
+                           <div className = "text pl-3">
+                              <p>
+                                <span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016
+                              </p>
+                           </div>
+                          </div>
+                          <div className = "dbox w-100 d-flex align-items-cneter">
+                         <div className = "icon d-flex align-items-center justify-content-center">
+                           <span className = "fa fa-phone"></span>
+                           </div>
+                           <div className = "text pl-3">
+                              <p>
+                                <span>Phone:</span>
+                                <a href="tel://123456789">+ 1235 7893 32 </a>
+                              </p>
+                           </div>
+                          </div>
+                          <div className = "dbox w-100 d-flex align-items-center">
+                         <div className = "icon d-flex align-items-center justify-content-center">
+                           <span className = "fa fa-paper-plane"></span>
+                           </div>
+                           <div className = "text pl-3">
+                              <p>
+                                <span>Email:</span> <a href = 'mailto:turntablellc3@gmail.com'>turtablellc3@gmail.com</a>
+                              </p>
+                           </div>
+                          </div>
+                          <div className = "dbox w-100 d-flex align-items-center">
+                         <div className = "icon d-flex align-items-center justify-content-center">
+                           <span className = "fa fa-globe"></span>
+                           </div>
+                           <div className = "text pl-3">
+                              <p>
+                                <span>Website:</span> <a href = '#'>turntable.com</a>
+                              </p>
+                           </div>
+                          </div>
+                        </div>
+                      </div>
+
+
+                  </div>
+                </div>
+              </div>  
+          </div> 
+      </div>
+   </section>
   )
 }
+
 
