26c65ac38e4e3f6d0367c0bb2473fe69088ed547
Ayush Jain <ayujain2004@gmail.com>
2022-04-05 22:07:03 -0400

    updated navigation bar

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,13 @@ export default function About() {
   return (
     <div>
     <h2>About Page</h2>
-    <p>Description of Turntables mission... </p>
+    <p>Welcome to turntable! Our mission is to allow artists to sell shares
+      of their royalties to their fans to allow artists to be more in control 
+      of their future through direct funding methods and allow for fans to be more involved
+      in their favorite artists sucess! If you are an artist that would be interested in learning 
+      more, please apply using our artist application and we will get in touch with you within 48 hours. 
+      Thanks! 
+    </p>
     
   </div>
   )
